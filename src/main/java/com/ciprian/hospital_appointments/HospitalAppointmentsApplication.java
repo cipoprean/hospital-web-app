17abb68857c9e90c33a120d4358242d5d5bf45a3
ciprianoprean <ciprian.oprean@stsnet.ro>
2026-04-21 16:35:55 +0300

    initial setup

---
+++ src/main/java/com/ciprian/hospital_appointments/HospitalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.ciprian.hospital_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciprian/hospital_appointments/HospitalAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciprian.hospital_appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
